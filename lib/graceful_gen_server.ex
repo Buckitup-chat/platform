39218f6acf0a24c8e6fc4433b04d09888a17bb3f
Serhii Lukianov <serhii.lukianov@litslink.com>
2023-04-20 08:26:00 +0300

    macro version

--- lib/graceful_gen_server.ex
+++ lib/graceful_gen_server.ex
@@ -3,6 +3,50 @@ defmodule GracefulGenServer do
     Handles init and terminate
   """
 
+  @callback on_init(args :: any) :: any
+  @callback on_exit(reason :: any, state :: any) :: any
+  @callback on_msg(msg :: any, state :: any) ::
+              {:noreply, new_state}
+              | {:noreply, new_state, timeout | :hibernate | {:continue, continue_arg :: term}}
+              | {:stop, reason :: term, new_state}
+            when new_state: term
+
+  @optional_callbacks on_msg: 2
+
+  defmacro __using__(start_opts) do
+    quote do
+      @behaviour GracefulGenServer
+      use GenServer
+
+      alias GracefulGenServer.Functions, as: Graceful
+
+      def start_link(args) do
+        GenServer.start_link(__MODULE__, args, unquote(Macro.escape(start_opts)))
+      end
+
+      @impl true
+      def init(args), do: Graceful.init(args, do: &on_init/1, as: __MODULE__)
+
+      @impl true
+      def handle_info(msg, state),
+        do:
+          Graceful.handle_info(msg, state,
+            on_exit: &on_exit/2,
+            on_msg: &on_msg/2,
+            as: __MODULE__
+          )
+
+      @impl true
+      def terminate(reason, state),
+        do: Graceful.terminate(reason, state, on_exit: &on_exit/2, as: __MODULE__)
+
+      @doc false
+      def on_msg(_msg, state), do: {:noreply, state}
+
+      defoverridable on_msg: 2
+    end
+  end
+
   defmodule Functions do
     require Logger
 
--- lib/platform/storage/mounter.ex
+++ lib/platform/storage/mounter.ex
@@ -2,42 +2,15 @@ defmodule Platform.Storage.Mounter do
   @moduledoc """
   Mounts on start, unmount on terminate
   """
-  use GenServer
+  use GracefulGenServer, timeout: :timer.minutes(1)
 
   require Logger
 
   alias Platform.Storage.Device
   alias Platform.Tools.Mount
 
-  alias GracefulGenServer.Functions, as: Graceful
-
-  def start_link(args) do
-    GenServer.start_link(__MODULE__, args, timeout: :timer.minutes(1))
-  end
-
   @impl true
-  def init(args), do: Graceful.init(args, do: &on_start/1, as: __MODULE__)
-
-  @impl true
-  def handle_info(msg, state),
-    do:
-      Graceful.handle_info(msg, state,
-        on_exit: &cleanup/2,
-        on_msg: &handle_msg/2,
-        as: __MODULE__
-      )
-
-  @impl true
-  def terminate(reason, state),
-    do: Graceful.terminate(reason, state, on_exit: &cleanup/2, as: __MODULE__)
-
-  defp handle_msg(some, state) do
-    Logger.info("info msg #{inspect(some)}")
-
-    {:noreply, state}
-  end
-
-  defp on_start([device, path, task_supervisor]) do
+  def on_init([device, path, task_supervisor]) do
     Task.Supervisor.async_nolink(task_supervisor, fn ->
       device
       |> Device.heal()
@@ -48,12 +21,20 @@ defmodule Platform.Storage.Mounter do
     {path, task_supervisor}
   end
 
-  defp cleanup(reason, {path, task_supervisor}) do
+  @impl true
+  def on_exit(reason, {path, task_supervisor}) do
     "mount cleanup #{path} #{inspect(reason)}" |> Logger.warn()
 
     Task.Supervisor.async_nolink(task_supervisor, fn ->
       Mount.unmount(path)
     end)
     |> Task.await()
+  end
+
+  @impl true
+  def on_msg(some, state) do
+    Logger.info("info msg #{inspect(some)}")
+
+    {:noreply, state}
   end
 end
