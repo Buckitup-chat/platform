5706128a15160998cc52a283645ffdfb8a817a48
Serhii Lukianov <serhii.lukianov@litslink.com>
2023-08-17 21:38:56 +0300

    451: weight sensor support

---
+++ lib/platform/sensor/weigh/balena_d700.ex
@@ -0,0 +1,50 @@
+defmodule Platform.Sensor.Weigh.BalenaD700 do
+  @moduledoc "Weight sensor for Balena D700"
+
+  defstruct pid: :no_d700_pid
+end
+
+defimpl Platform.Sensor.Weigh.Protocol, for: Platform.Sensor.Weigh.BalenaD700 do
+  alias Platform.Sensor.Weigh.Common
+  alias Platform.Sensor.Weigh.BalenaD700
+
+  @framing {Circuits.UART.Framing.Line, separator: "\r"}
+
+  @impl true
+  def open_port(%BalenaD700{}, port_name, port_opts) do
+    port_opts = Keyword.put(port_opts, :framing, @framing)
+
+    Common.open_port(port_name, port_opts)
+    |> case do
+      {:ok, pid} -> %BalenaD700{pid: pid}
+      other -> other
+    end
+  end
+
+  @impl true
+  def close_port(%BalenaD700{pid: pid}) do
+    Common.close_port(pid)
+    :ok
+  end
+
+  @impl true
+  def read(%BalenaD700{pid: pid}) do
+    status = Common.read_value(pid, "XS") |> String.trim()
+    weight = Common.read_value(pid, "XN") |> String.trim()
+    {:ok, weight, %{raw: status}}
+  end
+
+  @impl true
+  def read_message(%BalenaD700{} = sensor) do
+    case read(sensor) do
+      {:ok, weight, _status_map} ->
+        """
+        Weight: #{weight}
+        """
+        |> then(&{:ok, &1})
+
+      other ->
+        other
+    end
+  end
+end
---
+++ lib/platform/sensor/weigh/nci.ex
@@ -0,0 +1,65 @@
+defmodule Platform.Sensor.Weigh.NCI do
+  @moduledoc "UART wrapping for NCI protocol of weight sensor"
+
+  defstruct pid: :no_nci_pid
+end
+
+defimpl Platform.Sensor.Weigh.Protocol, for: Platform.Sensor.Weigh.NCI do
+  alias Platform.Sensor.Weigh.Common
+  alias Platform.Sensor.Weigh.NCI
+
+  @impl true
+  def open_port(%NCI{}, path, opts) do
+    Common.open_port(path, opts)
+    |> case do
+      {:ok, pid} -> {:ok, %NCI{pid: pid}}
+      other -> other
+    end
+  end
+
+  @impl true
+  def close_port(%NCI{pid: pid}) do
+    Common.close_port(pid)
+    :ok
+  end
+
+  @impl true
+  def read(%NCI{pid: pid}) do
+    with raw <- Common.read_value(pid, "W\r"),
+         {_, {:ok, weight, status_binary}} <- parse_response(raw),
+         {_, status} <- parse_status(status_binary) do
+      {:ok, weight, status}
+    end
+  end
+
+  @impl true
+  def read_message(%NCI{} = sensor) do
+    case read(sensor) do
+      {:ok, weight, status_map} ->
+        """
+        Weight: #{weight}
+
+        Status: #{summary(status_map)}
+        """
+        |> then(&{:ok, &1})
+
+      other ->
+        other
+    end
+  end
+
+  defp parse_response(binary),
+    do: {:parse_response, Chat.Sync.Weigh.NCI.parse_weight_response(binary)}
+
+  defp parse_status(binary), do: {:parse_status, Chat.Sync.Weigh.NCI.parse_status(binary)}
+
+  defp summary(map) do
+    map
+    |> Enum.filter(&elem(&1, 1))
+    |> Enum.map_join(" ", fn {key, _} ->
+      key
+      |> to_string()
+      |> String.trim_trailing("?")
+    end)
+  end
+end
