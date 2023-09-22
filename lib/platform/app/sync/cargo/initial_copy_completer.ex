e418c588c4d611ebb11bd6eba88e10de2c42387f
Serhii Lukianov <serhii.lukianov@litslink.com>
2023-09-22 11:11:12 +0300

    495: complete cargo room after scenario finished

--- lib/platform/app/sync/cargo/initial_copy_completer.ex
+++ lib/platform/app/sync/cargo/initial_copy_completer.ex
@@ -6,12 +6,13 @@ defmodule Platform.App.Sync.Cargo.InitialCopyCompleter do
   alias Chat.Sync.CargoRoom
 
   @impl true
-  def on_init(_opts) do
-    CargoRoom.mark_successful()
+  def on_init(opts) do
+    opts
   end
 
   @impl true
   def on_exit(_reason, _state) do
+    CargoRoom.mark_successful()
     CargoRoom.complete()
   end
 end
