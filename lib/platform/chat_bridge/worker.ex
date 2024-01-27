88c9066a9178248f6994f33591483ff274a3ddbf
Serhii Lukianov <serhii.lukianov@litslink.com>
2024-01-27 10:00:26 +0200

    582: ignore failures in chat bridge

--- lib/platform/chat_bridge/worker.ex
+++ lib/platform/chat_bridge/worker.ex
@@ -53,7 +53,8 @@ defmodule Platform.ChatBridge.Worker do
       {:lan_set_profile, profile} ->
         Logic.set_lan_profile(profile)
 
-      {:lan_ip_and_mask, pid} -> {:to, pid, {:range, {Lan.get_ip_address(), Lan.get_ip_mask()}}}
+      {:lan_ip_and_mask, pid} ->
+        {:to, pid, {:range, {Lan.get_ip_address(), Lan.get_ip_mask()}}}
 
       :get_device_log ->
         Logic.get_device_log()
@@ -76,6 +77,8 @@ defmodule Platform.ChatBridge.Worker do
     |> respond()
 
     noreply(state)
+  rescue
+    _ -> noreply(state)
   end
 
   defp noreply(x), do: {:noreply, x}
